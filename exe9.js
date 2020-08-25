f2fc8e977c1d9c459bcd53939e4bf02940054d68
Stela <you@example.com>
2020-08-24 22:15:24 -0300

    exe9 realizado

---
+++ exe9.js
@@ -0,0 +1,13 @@
+function exe9(numero){
+    var soma = 0
+    for (var i =numero; i <= 1; i--){// é i -- pq vai diminuindo
+        if (numero % i == 0){
+            soma = soma + i //soma dos divisores
+        }
+    }
+    console.log(`Soma dos divisores é ${}`)
+}
+/*9/ 9
+9 /8
+9/7 foi diminuindo os divisores */
+
